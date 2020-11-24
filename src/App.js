e8203f42753e9f186835c1dd4a3c065ffa50342f
Eugene Davidov <14vettona@rambler.ru>
2020-11-24 15:07:42 +0200

    Added basic markup and local state.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  let [number, setNumber] = useState("0");
+  let [fakeNumber, setFakeNumber] = useState("0");
+  let [result, setResult] = useState("");
+
+  const buttonValues = ["AC", "+/-", "%", "/", "mc", "mr", "m-", "m+", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="];
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+
+  const handleClick = e => {
+    let val = e.target.value;
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="number">{number || fakeNumber}</div>
+        <div className="result">{result}</div>
+        {buttonValues.map(val => <button value={val} onClick={handleClick}>{val}</button>)}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
